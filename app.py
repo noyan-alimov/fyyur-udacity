db3a8cfb3689980421cd44cb9e597e3e1d7f6fa3
Noyan Alimov <noyan.alimov@gmail.com>
2020-07-01 12:13:14 +0100

    implemented search functionality for venues and artists in controllers

--- app.py
+++ app.py
@@ -148,15 +148,24 @@ def search_venues():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for Hop should return "The Musical Hop".
     # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    search_term = request.form.get('search_term', '')
+    venues = Venue.query.filter(
+        Venue.name.ilike('%{}%'.format(search_term))).all()
+    data = []
+
+    for venue in venues:
+        obj = {
+            'id': venue.id,
+            'name': venue.name,
+            'num_upcoming_shows': len(venue.shows)
+        }
+        data.append(obj)
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(venues),
+        "data": data
     }
-    return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    return render_template('pages/search_venues.html', results=response, search_term=search_term)
 
 
 @app.route('/venues/<int:venue_id>')
@@ -311,14 +320,24 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term', '')
+    artists = Artist.query.filter(
+        Artist.name.ilike('%{}%'.format(search_term))).all()
+    data = []
+
+    for artist in artists:
+        obj = {
+            'id': artist.id,
+            'name': artist.name,
+            'num_upcoming_shows': len(artist.shows)
+        }
+        data.append(obj)
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(artists),
+        "data": data
     }
+
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
